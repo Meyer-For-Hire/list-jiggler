99fa7384b71114be3f6fd9b78dc4fe20b2139d00
Jim Meyer <jim@geekdaily.org>
2025-03-07 16:28:10 -0800

    Add proper routing

---
+++ app/list/[encoded]/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { encoded: string } }
+) {
+  return NextResponse.next();
+} 
